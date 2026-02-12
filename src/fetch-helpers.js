e9cf99ee7ef142bd33d7bde2bf999896898dd068
Emily Vu <evu725@gmail.com>
2026-02-12 15:59:35 -0500

    update progress

--- src/fetch-helpers.js
+++ src/fetch-helpers.js
@@ -3,11 +3,34 @@ export const fetchData = (url) => {
 };
 
 export const getProducts = () => {
-
+    return fetch('https://dummyjson.com/products')
+        .then((response) => {
+            if(!response.ok) {
+                throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .then((data) => {
+            return { data: data.products , error: null };
+        })
+        .catch((error) => {
+            console.error(error.message);
+            return { data: null, error: error };
+        });
 };
 
 export const getProductById = (id) => {
-
+    return fetch(`https://dummyjson.com/products${id}`)
+        .then((response) => {
+            if(!response.ok) {
+                throw Error(`Fetch failed. ${response.status} ${response.statusText}`);
+            }
+            return response.json();
+        })
+        .catch((error) => {
+            console.error(error.message);
+            return { data: null, error: error };
+        });
 };
 
 export const searchProducts = (query) => {
